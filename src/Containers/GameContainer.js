89751e1b3ac91383f66c2447ebc53643d630a24e
mkay0961 <mkay0961@gmail.com>
2019-03-22 14:08:19 -0400

    winning stuff

--- src/Containers/GameContainer.js
+++ src/Containers/GameContainer.js
@@ -1,3 +1,12 @@
+//add four players
+
+//then
+
+//ADDD INSTULTS
+
+
+
+
 
 import React, {Component} from 'react'
 import UserHandContainer from './UserHandContainer'
@@ -46,7 +55,6 @@ class GameContainer extends Component {
   }
 
   drawcard=()=>{
-    console.log(this.state.turn);
     if(this.state.deck.length > 0 ){
       let drawnCard = this.state.deck.pop()
 
@@ -85,127 +93,52 @@ class GameContainer extends Component {
 
   }
   changeTurn(){
+    this.checkWin()
+    //post to backend
     this.setState({
       turn: ((this.state.turn +1) % this.state.players.length)
     })
   }
 
+  checkWin(){
+    let cardCount = this.state.players.map((player)=>{
+      return player.cards.length
+    })
+    if(cardCount.includes(0)){
+      alert(`WINNER ${this.state.players[this.state.turn].name}`)
+    }
+  }
 
   onSelectCardClick = (card) =>{
     if(this.cardlogic(card)){
       this.playCard(card)
       this.changeTurn()
       setTimeout(this.compTurn, 1000)
-
     }
   }
 
-    compTurn =() => {
-      let potentialMoves = this.state.players[this.state.turn].cards.filter(card => card.Number === this.state.active_card.Number || card.Color === this.state.active_card.Color)
+  compTurn =() => {
+    let potentialMoves = this.state.players[this.state.turn].cards.filter(card => card.Number === this.state.active_card.Number || card.Color === this.state.active_card.Color)
 
-      while (potentialMoves.length === 0) {
-        //drawcard
-        if(this.state.deck.length > 0){
-          this.drawcard()
-        }
-        potentialMoves = this.state.players[this.state.turn].cards.filter(card => card.Number === this.state.active_card.Number || card.Color === this.state.active_card.Color)
+    while (potentialMoves.length === 0) {
+      //drawcard
+      if(this.state.deck.length > 0){
+        this.drawcard()
       }
-
-      //change to pic random
-      let card = potentialMoves[0]
-
-      this.playCard(card)
-
-      this.changeTurn()
+      potentialMoves = this.state.players[this.state.turn].cards.filter(card => card.Number === this.state.active_card.Number || card.Color === this.state.active_card.Color)
     }
 
+    //change to pic random
+    let card = potentialMoves[0]
 
-    // e.persist()
-    // console.log("you clicked card", card);
-    // console.log(this.cardlogic(card));
-    // console.log((e === undefined)? "comp":"user")
-    // if(this.state.turn == 0){
-    //   if(this.cardlogic(card)){
-    //     let newplayerhand = this.state.userhand
-    //     newplayerhand = newplayerhand.filter((c)=>c!==card)
-    //
-    //     this.setState({
-    //       deck: [this.state.active_card, ...this.state.deck],
-    //       active_card: card,
-    //       userhand: newplayerhand,
-    //       turn: 1
-    //     })
-    //     setTimeout(this.drawcard, 1000)}
-    //   }
-    //   else if (this.state.turn === 0) {
-    //     console.log("COMP TURN");
-    //   }
+    this.playCard(card)
 
-
-
-
-  // compTurn = () => {
-  //   let potentialMoves = this.state.comp1hand.filter(card => card.Number === this.state.active_card.Number || card.Color === this.state.active_card.Color)
-  //
-  //   while (potentialMoves.length === 0) {
-  //     //drawcard
-  //     if(this.state.deck.length > 0){
-  //       let drawnCard = this.state.deck.pop()
-  //       let updatedDeck = this.state.deck
-  //       updatedDeck = updatedDeck.filter((c)=>c!==drawnCard)
-  //
-  //       //shuffle
-  //       updatedDeck.sort(() => Math.random() - 0.5);
-  //       updatedDeck.sort(() => Math.random() - 0.5);
-  //
-  //       this.setState({
-  //         deck: updatedDeck,
-  //         comp1hand: [...this.state.comp1hand, drawnCard]
-  //       })
-  //     }
-  //     potentialMoves = this.state.comp1hand.filter(card => card.Number === this.state.active_card.Number || card.Color === this.state.active_card.Color)
-  //   }
-  //   let card = potentialMoves[0]
-  //
-  //   let newCompHand = this.state.comp1hand
-  //   newCompHand = newCompHand.filter((c)=>c!==card)
-  //   //random from that array
-  //   //set that to top card
-  //   this.setState({
-  //     deck: [this.state.active_card, ...this.state.deck],
-  //     active_card: card,
-  //     comp1hand: newCompHand,
-  //     turn: 0,
-  //
-  //   })
-  //   //change turn
-  //
-  //
-  //
-  // }
+    this.changeTurn()
+  }
 
   handleActiveCard = (card) =>{
     console.log("you clicked the active card", card);
   }
-  // handleDeckClick = () =>{
-  //   console.log(this.state.deck.length);
-  //   if(this.state.deck.length > 0){
-  //     let drawnCard = this.state.deck.pop()
-  //     let updatedDeck = this.state.deck
-  //     updatedDeck = updatedDeck.filter((c)=>c!==drawnCard)
-  //
-  //     //shuffle
-  //     updatedDeck.sort(() => Math.random() - 0.5);
-  //     updatedDeck.sort(() => Math.random() - 0.5);
-  //
-  //     this.setState({
-  //       deck: updatedDeck,
-  //       userhand: [...this.state.userhand, drawnCard]
-  //     })
-  //   }else{
-  //     console.log("no more cards in deck");
-  //   }
-  // }
 
   render() {
 
